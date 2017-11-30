ee74156b3bc739e0ff6100963a591db7ac2f94d5
Alex Oviedo <alexfabi1@gmail.com>
2017-12-01 00:01:42 +0100

    improvements dashboard, confirmation..

--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -6,6 +6,7 @@ import React, { PureComponent } from 'react'
 
 import { connect } from 'react-redux'
 
+import { Grid, Row , Col } from 'react-bootstrap';
 
 const mapStateToProps = ( state, ownProps ) => {
 
@@ -24,21 +25,41 @@ class Dashboard extends PureComponent {
     }
     render(){
         return(
-            <section id="dashboard" className="dashboard-section section" >
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h2 className="section-heading wow fadeIn" data-wow-duration="1s">
-                                Welcome { this.props.profile.firstName } to Real Safe!
+            <section id="dashboard" className="dashboard-section section">
+                <Grid>
+                    <Row>
+                        <Col md={ 5 } mdOffset={ 1 }>
+                            <h2 className="section-heading">
+                                Hi { this.props.profile.firstName }
                             </h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-10 col-md-offset-1">
-                             <p> Will contact you very soon once the ICO is ready </p>
-                        </div>
-                    </div>
-                </div>
+                            <p className="buy-tokens">BUY TOKENS</p>
+                            <div className="box-tokens">
+                                <p>Address will be available at the start of the pre-ICO, on March 1st at 10AM UTC.</p>
+                                <p>PRE-ICO STARTS IN</p>
+                                <div id ="getting-started" className="getting-started"> </div>
+                            </div>
+                        </Col>
+                        <Col md={ 5 }>
+                            <Row>
+                                <Col className="balance" md={ 12 }>
+                                    <p className="text"> YOUR BALANCE </p>
+                                    <p className="number"> 0 RST </p>
+                                </Col>
+                                <Col className="price" md={ 12 }>
+                                    <p className="text"> RST TOKEN PRICE </p>
+                                    <p className="text-secondary">First 10,000,000 RST - 30% discount, 1 RST </p>
+                                    <p className="number">0,87 $ (Minimum pre-purchase: 1 RSO + 1 asset = 1100 RST ← 3.19 ETH </p>
+                                    <p className="text-secondary">Next 15,000,000 RST - 15% discount, 1 RST </p>
+                                    <p className="number">1,06 $ (Minimum pre-purchase of 1 RSO + 1 asset = 1100 RST ← 3.89 ETH </p>
+                                    <p className="text-secondary">Next 25,000,000 RST - discount of 8%, 1 RST  </p>
+                                    <p className="number">1,15 $ (Minimum pre-purchase of 1 RSO + 1 asset = 1100 RST ← 4.22 ETH </p>
+                                    <p className="text-secondary">Final 25,000,000 RST - 1 RST </p>
+                                    <p className="number">1,25 $ (Minimum pre-purchase of 1 RSO + 1 asset = 1100 RST ← 4.58 ETH</p>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Grid>
             </section>
         )
     }
--- src/scenes/NeedConfirmationEmailSent/index.js
+++ src/scenes/NeedConfirmationEmailSent/index.js
@@ -4,33 +4,113 @@ import Ru from 'rutils'
 
 import React, { PureComponent } from 'react'
 
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
+
+import autobind from 'autobind-decorator'
 
 import B from 'bluebird'
 
+import { auth } from '../../services'
+
+import LaddaButton, { S, ZOOM_IN } from 'react-ladda';
+
+import 'url-search-params-polyfill'
+
+import Alert from 'react-s-alert';
 
 @withRouter
 class NeedConfirmationEmailSent extends PureComponent {
 
     constructor(props) {
         super(props);
+        this.state = {
+          loading: false,
+        }
+    }
+
+    onResend(){
+      let search = new URLSearchParams (this.props.location.search)
+      this.setState({
+          loading: true
+      });
+      let email = search.get('email')
+
+      console.log('Resend to this email', email);
+      auth
+      .resendConfirmationLink(email)
+      .then( r => {
+        console.log('Daniel::: ', r)
+
+        Alert.success(
+            `Email resent successfully`,
+            {
+                offset: 80,
+                timeout: 7000,
+                position: 'top',
+                effect: 'flip'
+            }
+        )
+
+        this.setState({
+            loading: false
+        });
+      })
+      .catch( err => {
+        Alert.error(
+            err.description,
+            {
+                offset: 50,
+                position: 'top-right',
+                effect: 'slide',
+                html: false,
+                timeout: 5000,
+            }
+        )
+        this.setState({
+            loading: false
+        });
+
+      })
+
     }
 
 
     render(){
         return(
             <section id="confirmation" className="confirmation-section section" >
-                <div className="container text-center">
+                <div className="container">
                     <div className="row">
-                        <div className="col-md-12">
+                        <div className="col-md-12 text-center">
+                            <Alert stack={{limit: 3}}/>
+                        </div>
+                        <div className="col-md-6 col-md-offset-3">
+                            <div className="number">
+                                <i className="fa fa-check" aria-hidden="true"></i>
+                            </div>
                             <h2 className="section-heading wow fadeIn" data-wow-duration="1s">
-                                Confirmation email successfully sent...
+                                Almost there!
                             </h2>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-10 col-md-offset-1">
-                             <p> A confirmation email has been sent to your email. Please go your email for verifying your account.  </p>
+                        <div className="col-md-6 col-md-offset-3">
+                             <p>You still need to confirm your account.<br/>
+                             If there is not account associated with this email, you will receive instructions in your inbox. </p>
+                             <p> If you can't find the email, click here to resend: </p>
+                             <div className="btn-contain">
+                                 <LaddaButton
+                                     className="btn btn-login"
+                                     loading={ this.state.loading }
+                                     data-color="#eee"
+                                     data-size={ S }
+                                     onClick= { this.onResend }
+                                     data-style={ ZOOM_IN }
+                                     data-spinner-size={ 30 }
+                                     data-spinner-color="#ddd"
+                                     data-spinner-lines={ 12 }
+                                 >Resend verification email
+                                 </LaddaButton>
+                             </div>
                         </div>
                     </div>
                 </div>
